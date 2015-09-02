f564682c3ce7d39a02a5207887c2376dace00c30
Jakob Rindom <rindom@gmail.com>
2015-09-02 14:28:27 +0200

    Issue #INNOVATION-19 by rindom: Changed setup to PHP with a  _menus.php and a  _footer.php files. Updated the box layout to itk-boxlist design

---
+++ innovationscenteret/_menus.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Innovationscenteret</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href='//fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/menu_toggle.js"></script>
+</head>
+<body>
+<header role="banner">
+    <nav class="nav">
+        <div class="nav--logo"><a href="#">Logo</a></div>
+        <div class="nav--links">
+            <a href="#" class="nav--link">MenuPunkt1</a>
+            <a href="#" class="nav--link">MenuPunkt2</a>
+            <a href="#" class="nav--link">MenuPunkt3</a>
+            <a href="#" class="nav--link">Menupunkt4</a>
+        </div>
+        <button class="nav--menu c-hamburger" onclick="menu_toggle()">
+            <span>toggle menu</span>
+        </button>
+    </nav>
+    <div class="overlay" onclick="menu_toggle()"></div>
+    <nav class="hamburger-menu">
+        <button class="c-hamburger c-hamburger--htx" onclick="menu_toggle()">
+            <span>toggle menu</span>
+        </button>
+        <div class="hamburger-links">
+            <a href="#" class="nav-link">MenuPunkt1</a>
+            <a href="#" class="nav-link">MenuPunkt2</a>
+            <a href="#" class="nav-link">MenuPunkt3</a>
+            <a href="#" class="nav-link">Menupunkt4</a>
+        </div>
+    </nav>
+</header>
