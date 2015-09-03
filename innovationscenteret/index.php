5496e01b9bd78be7bd2780c962e2d16845196346
Brian Jensen <hallo@magnify.dk>
2015-09-03 10:06:21 +0200

    #INNOVATION-20 by magnify: Added header big image to frontpage

--- innovationscenteret/index.php
+++ innovationscenteret/index.php
@@ -1,62 +1,20 @@
 <?php
   include("_menus.php")
 ?>
-
+<header class="header" role="banner" id="home">
+  <div class="header--inner">
+    <h1 class="header--big">The big header</h1>
+    <p class="header--sub">This is some optional text we can either keep or delete</p>
+    <div class="header--button-wrapper"><a href="#" class="header--button-link" data-scroll>Look at me!</a></div>
+  </div>
+</header>
 <main role="main">
   <div class="content">
     <h1>This is a header</h1>
-
-
-      <!-- ITK boxlist -->
-      <div class="itk-boxlist">
-        <article class="itk-boxlist--item">
-          <header>
-            <h2 class="itk-boxlist--header">Vind to billetter Lars Frost, Niels Frank og Kamilla Löfström</h2>
-            <p class="itk-boxlist--type">Forelæsning</p>
-            <div class="itk-boxlist--metadata">
-              <span class="itk-boxlist--date">14. maj 2015</span>
-              <span class="itk-boxlist--time">10:00 - 12:00</span>
-              <span class="itk-boxlist--location">Multisalen</span>
-            </div>
-          </header>
-          <figure><img src="http://placehold.it/200x200"></figure>
-        </article>
-      </div>
-
-      <!-- ITK boxlist side by side -->
-      <div class="itk-boxlist is-side-by-side">
-        <article class="itk-boxlist--item">
-          <header>
-            <h2 class="itk-boxlist--header">Vind to billetter Lars Frost, Niels Frank og Kamilla Löfström</h2>
-            <p class="itk-boxlist--type">Forelæsning</p>
-            <div class="itk-boxlist--metadata">
-              <span class="itk-boxlist--date">14. maj 2015</span>
-              <span class="itk-boxlist--time">10:00 - 12:00</span>
-              <span class="itk-boxlist--location">Multisalen</span>
-            </div>
-          </header>
-          <figure><img src="http://placehold.it/200x200"></figure>
-        </article>
-        <article class="itk-boxlist--item">
-          <header>
-            <h2 class="itk-boxlist--header">Vind to billetter Lars Frost, Niels Frank og Kamilla Löfström</h2>
-            <p class="itk-boxlist--type">Forelæsning</p>
-            <div class="itk-boxlist--metadata">
-              <span class="itk-boxlist--date">14. maj 2015</span>
-              <span class="itk-boxlist--time">10:00 - 12:00</span>
-              <span class="itk-boxlist--location">Multisalen</span>
-            </div>
-          </header>
-          <figure><img src="http://placehold.it/200x200"></figure>
-        </article>
-      </div>
-
-
-
   </div>
 </main>
 
 <?php
-include("_footer.php")
+  include("_footer.php")
 ?>
 
