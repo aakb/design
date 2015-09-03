ccead1f52f0a511d3a69096aa8c8578729b6c67b
Jakob Rindom <rindom@gmail.com>
2015-09-03 13:58:54 +0200

    Issue #INNOVATION-19 by rindom: Updated frontpage with lead and three side-by-side views. Added a page with just side by side wievs

--- innovationscenteret/index.php
+++ innovationscenteret/index.php
@@ -1,6 +1,7 @@
 <?php
   include("_menus.php")
 ?>
+
 <header class="header" role="banner" id="home">
   <div class="header--inner">
     <h1 class="header--big">The big header</h1>
@@ -9,8 +10,42 @@
   </div>
 </header>
 <main role="main">
-  <div class="content">
-    <h1>This is a header</h1>
+  <div class="content frontpage">
+    <p class="lead">Est notare quam littera gothica quam nunc putamus! Autem vel eum iriure dolor in hendrerit: in vulputate velit esse molestie.
+
+    <div class="overview is-side-by-side">
+      <article class="overview--item">
+        <header>
+          <h2 class="overview--header">This is a header</h2>
+          <p class="overview--text">Quod mazim placerat facer possim assum typi non habent claritatem? Eorum claritatem Investigationes demonstraverunt lectores legere me lius quod. Dolore te feugait nulla facilisi nam liber.
+          </p>
+          <a href="sitethree.php" class="overview--link">This is a link</a>
+        </header>
+        <figure><img src="https://unsplash.it/200/200/?random" class="overview--image"></figure>
+      </article>
+
+      <article class="overview--item">
+        <header>
+          <h2 class="overview--header">This is a header</h2>
+          <p class="overview--text">Quod mazim placerat facer possim assum typi non habent claritatem? Eorum claritatem Investigationes demonstraverunt lectores legere me lius quod. Dolore te feugait nulla facilisi nam liber.
+          </p>
+          <a href="sitethree.php" class="overview--link">This is a link</a>
+        </header>
+        <figure><img src="https://unsplash.it/200/200/?random" class="overview--image"></figure>
+      </article>
+
+      <article class="overview--item">
+        <header>
+          <h2 class="overview--header">This is a header</h2>
+          <p class="overview--text">Quod mazim placerat facer possim assum typi non habent claritatem? Eorum claritatem Investigationes demonstraverunt lectores legere me lius quod. Dolore te feugait nulla facilisi nam liber.
+          </p>
+          <a href="sitethree.php" class="overview--link">This is a link</a>
+        </header>
+        <figure><img src="https://unsplash.it/200/200/?ran.o" class="overview--image"></figure>
+      </article>
+    </div>
+
+
   </div>
 </main>
 
