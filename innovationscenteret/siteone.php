94d019df4c004e08eebb1f9f3bfff54a32737baa
Jakob Rindom <rindom@gmail.com>
2015-09-03 10:25:32 +0200

    Issue #INNOVATION-19 by rindom: Added overview page on sitetwo

--- innovationscenteret/siteone.php
+++ innovationscenteret/siteone.php
@@ -5,8 +5,42 @@ include("_menus.php")
 <main role="main">
     <div class="content">
         <h1>This is a header siteone</h1>
+        <p class="lead">Est notare quam littera gothica quam nunc putamus! Autem vel eum iriure dolor in hendrerit: in vulputate velit esse molestie.
+        </p>
 
 
+        <div class="overview">
+            <article class="overview--item">
+                <header>
+                    <h2 class="overview--header">This is a header</h2>
+                    <p class="overview--text">Quod mazim placerat facer possim assum typi non habent claritatem? Eorum claritatem Investigationes demonstraverunt lectores legere me lius quod. Dolore te feugait nulla facilisi nam liber, tempor cum soluta nobis eleifend option congue nihil. Legunt saepius claritas est etiam processus dynamicus qui? Luptatum zzril delenit augue duis, imperdiet doming id insitam est usus legentis in iis qui. Elit sed diam nonummy nibh euismod tincidunt ut. In vulputate velit esse molestie consequat vel illum dolore eu.
+                    </p>
+                    <a href="sitethree.php" class="overview--link">This is a link</a>
+                </header>
+                <figure><img src="http://placehold.it/200x200" class="overview--image"></figure>
+            </article>
+
+            <article class="overview--item">
+                <header>
+                    <h2 class="overview--header">This is a header</h2>
+                    <p class="overview--text">Quod mazim placerat facer possim assum typi non habent claritatem? Eorum claritatem Investigationes demonstraverunt lectores legere me lius quod. Dolore te feugait nulla facilisi nam liber, tempor cum soluta nobis eleifend option congue nihil. Legunt saepius claritas est etiam processus dynamicus qui? Luptatum zzril delenit augue duis, imperdiet doming id insitam est usus legentis in iis qui. Elit sed diam nonummy nibh euismod tincidunt ut. In vulputate velit esse molestie consequat vel illum dolore eu.
+                    </p>
+                    <a href="sitethree.php" class="overview--link">This is a link</a>
+                </header>
+                <figure><img src="http://placehold.it/200x200" class="overview--image"></figure>
+            </article>
+
+            <article class="overview--item">
+                <header>
+                    <h2 class="overview--header">This is a header</h2>
+                    <p class="overview--text">Quod mazim placerat facer possim assum typi non habent claritatem? Eorum claritatem Investigationes demonstraverunt lectores legere me lius quod. Dolore te feugait nulla facilisi nam liber, tempor cum soluta nobis eleifend option congue nihil. Legunt saepius claritas est etiam processus dynamicus qui? Luptatum zzril delenit augue duis, imperdiet doming id insitam est usus legentis in iis qui. Elit sed diam nonummy nibh euismod tincidunt ut. In vulputate velit esse molestie consequat vel illum dolore eu.
+                    </p>
+                    <a href="sitethree.php" class="overview--link">This is a link</a>
+                </header>
+                <figure><img src="http://placehold.it/200x200" class="overview--image"></figure>
+            </article>
+
+        </div>
 
 
     </div>
