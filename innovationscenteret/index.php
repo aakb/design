edfa5f0320d0a37221b75be133ac131c77b9ed05
Brian Jensen <hallo@magnify.dk>
2015-09-29 11:05:25 +0200

    Issue #INNOVATION-63 by magnify: Styling of header

--- innovationscenteret/index.php
+++ innovationscenteret/index.php
@@ -5,9 +5,10 @@
 
 <div class="header" role="banner" id="home">
   <div class="header--inner">
-    <h1 class="header--big">The big header</h1>
-    <p class="header--sub">This is some optional text we can either keep or delete</p>
-  <div class="header--button-wrapper"><a href="#" class="header--button-link" data-scroll>Look at me!</a></div>
+    <h1 class="header--big">Hej.</h1>
+    <p class="header--lead">Vi er Center for Innovation i Aarhus, vi understøtter og styrker arbejdet med innovation i Aarhus.</p>
+    <p class="header--sub">Vi kan hjælpe jer med servicedesign, research, processer og meget andet. Tøv ikke med at skrive til os for at høre mere!</p>
+  <div class="header--button-wrapper"><a href="#" class="header--button-link" data-scroll>Kontakt os</a></div>
 </div>
 </div>
 <main role="main">
