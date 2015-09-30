a64cd7f4aa0cac4d86b7085e3f5ddbbca1ce9073
Brian Jensen <hallo@magnify.dk>
2015-09-30 08:57:28 +0200

    Issue #INNOVATION-63 by magnify: Design

--- innovationscenteret/index.php
+++ innovationscenteret/index.php
@@ -7,10 +7,9 @@
   <div class="header--inner">
     <h1 class="header--big">Hej.</h1>
     <p class="header--lead">Vi er Center for Innovation i Aarhus, vi understøtter og styrker arbejdet med innovation i Aarhus.</p>
-    <p class="header--sub">Vi kan hjælpe jer med servicedesign, research, processer og meget andet. Tøv ikke med at skrive til os for at høre mere!</p>
-  <div class="header--button-wrapper">
-    <a href="#" class="header--button-link is-secondary">Ydelser</a>
-    <a href="#" class="header--button-link is-primary">Kontakt os</a>
+    <p class="header--sub">Vi kan hjælpe jer med servicedesign, research, processer og meget andet. Tøv ikke med at <a href="#">skrive til os</a> eller lær os at kende først!</p>
+    <div class="header--button-wrapper">
+    <a href="#" class="header--button-link is-primary">Ydelser</a>
   </div>
 </div>
 </div>
@@ -21,23 +20,31 @@
       <header>
         <h2 class="itk-boxlist--header">Faciliteter</h2>
       </header>
-      <figure><img src="http://placehold.it/500x500"></figure>
+      <figure><img src="images/icons/faciliteter_sort.svg"></figure>
     </article>
     <article class="itk-boxlist--item">
       <header>
         <h2 class="itk-boxlist--header">Prototyper</h2>
       </header>
-      <figure><img src="http://placehold.it/500x500"></figure>
+      <figure><img src="images/icons/faciliteter_sort.svg"></figure>
     </article>
     <article class="itk-boxlist--item">
       <header>
         <h2 class="itk-boxlist--header">Projektsamarbejde</h2>
       </header>
-      <figure><img src="http://placehold.it/500x500"></figure>
+      <figure><img src="images/icons/faciliteter_sort.svg"></figure>
     </article>
   </div>
 </main>
-
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 <?php
   include '_footer.php';
 ?>
