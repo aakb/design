dd0e78bca2c9e5ddd4fdb76625d4c7c4e5b3f0c4
Jakob Rindom <rindom@gmail.com>
2015-10-01 10:01:02 +0200

    Issue #INNOVATION-? by rindom: Cleaned up footer css.

--- innovationscenteret/_footer.php
+++ innovationscenteret/_footer.php
@@ -2,7 +2,6 @@
   <div class="footer--content">
     <div class="footer--mail">
       <a href="mailto:cfi@aarhus.dk">cfi@aarhus.dk</a>
-
     </div>
     <div class="footer--socialmedia">
       <a href="instagram.com"><figure><?php include 'includes/icons/instagram_icon.php'; ?></figure></a>
