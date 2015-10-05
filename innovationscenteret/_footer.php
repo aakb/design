bd7e51af8dbc54435147f2d202a8901051fff5b3
Brian Jensen <hallo@magnify.dk>
2015-10-05 13:00:12 +0200

    Issue #INNOVATION-56 by magnify: Styling of footer

--- innovationscenteret/_footer.php
+++ innovationscenteret/_footer.php
@@ -3,10 +3,10 @@
     <div class="footer--mail">
       Dokk1, Hack Kampmanns Plads 2 8000 Aarhus C - <a href="mailto:cfi@aarhus.dk">cfi@aarhus.dk</a>
     </div>
-    <div class="footer--socialmedia">
-      <a href="instagram.com"><figure class="footericons"><img src="images/twitter.png"></figure></a>
-      <a href="instagram.com"><figure class="footericons"><img src="images/instagram.png"></figure></a>
-      <a href="instagram.com"><figure class="footericons"><img src="images/linkedin.png"></figure></a>
+    <div class="footer--social">
+      <a href="#"><figure class="footer--icons"><img src="images/twitter.png"></figure></a>
+      <a href="#"><figure class="footer--icons"><img src="images/instagram.png"></figure></a>
+      <a href="#"><figure class="footer--icons"><img src="images/linkedin.png"></figure></a>
     </div>
   </div>
 </footer>
