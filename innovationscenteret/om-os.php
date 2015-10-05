eeb5318c787044ef82e750ebcadbb4a45c0033cd
Brian Jensen <hallo@magnify.dk>
2015-10-05 14:57:56 +0200

    Issue #INNOVATION-56 by magnify: Added links

--- innovationscenteret/om-os.php
+++ innovationscenteret/om-os.php
@@ -18,27 +18,27 @@
   <div class="itk-boxlist">
     <article class="itk-boxlist--item">
       <header>
-        <h2 class="itk-boxlist--header">Something</h2>
+        <h2 class="itk-boxlist--header"><a href="#">Something</a></h2>
         <p>Til dig der gerne vil dele din erfaring og inspireres af andre kollegaer og eksperter tilbyder Center for
         Innovation i Aarhus deltagelse i vores innovationsnetværk. Her møder du kollegaer i Aarhus Kommune, der
         arbejder med innovation ligesom dig.</p>
         <a href="netvaerk.php">Læs om netværk</a>
       </header>
-      <figure><img src="https://unsplash.it/500/500/?random"></figure>
+      <figure><a href="#"><img src="https://unsplash.it/500/500/?random"></a></figure>
     </article>
     <article class="itk-boxlist--item">
       <header>
-        <h2 id="network" class="itk-boxlist--header">Prototyper</h2>
+        <h2 id="network" class="itk-boxlist--header"><a href="#">Prototyper</a></h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Hi omnes lingua, institutis, legibus inter se differunt. Cum ceteris in veneratione tui montes, nascetur mus.</p>
       </header>
-      <figure><img src="https://unsplash.it/500/600/?random"></figure>
+      <figure><a href="#"><img src="https://unsplash.it/500/600/?random"></a></figure>
     </article>
     <article class="itk-boxlist--item">
       <header>
-        <h2 class="itk-boxlist--header">Noget andet</h2>
+        <h2 class="itk-boxlist--header"><a href="#">Noget andet</a></h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Hi omnes lingua, institutis, legibus inter se differunt. Cum ceteris in veneratione tui montes, nascetur mus.</p>
       </header>
-      <figure><img src="https://unsplash.it/500/700/?random"></figure>
+      <figure><a href="#"></a><img src="https://unsplash.it/500/700/?random"></a></figure>
     </article>
   </div>
 </main>
