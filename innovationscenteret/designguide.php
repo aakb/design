a62c19da49f01206ef882264c800f6b0f5f7b991
Brian Jensen <hallo@magnify.dk>
2015-10-07 13:16:42 +0200

    Issue #INNOVATION-87 by magnify: Added first previous example to pager

--- innovationscenteret/designguide.php
+++ innovationscenteret/designguide.php
@@ -63,22 +63,7 @@
   <br><br>
   <h2>Pager</h2>
   <fieldset>
-    <nav class="pager">
-      <ul class="pager__items">
-        <li class="pager__item is-active">
-          <a href="/news" title="Nuværende side">1</a>
-        </li>
-        <li class="pager__item">
-          <a href="#" title="Go to page 2">2</a>
-        </li>
-        <li class="pager__item pager__item--next">
-          <a href="#" title="Gå til næste side" rel="next"><span class="visually-hidden">Næste</span></a>
-        </li>
-        <li class="pager__item pager__item--last">
-          <a href="#" title="Gå til sidste side"><span>Sidste</span></a>
-        </li>
-      </ul>
-    </nav>
+    <?php include 'includes/_pager.php'; ?>
   </fieldset>
   <!-- Cookies -->
   <div id="js-cookies" class="cookies">
