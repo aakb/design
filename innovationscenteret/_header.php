c4e0b16c5bc8d2707cce926be433551aa17ba421
Brian Jensen <hallo@magnify.dk>
2015-10-07 14:59:59 +0200

    Issue #INNOVATION-87 by magnify: Optimized for IE9

--- innovationscenteret/_header.php
+++ innovationscenteret/_header.php
@@ -6,6 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <!--[if IE 9]>
+  <link href="css/ie9.css" rel="stylesheet" type="text/css">
+  <![endif]-->
   <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900,300,100,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="js/menu-toggle.js"></script>
