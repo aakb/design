a2abd7c60e46c01ae002c23f3b3a2e8a95efec78
Brian Jensen <hallo@magnify.dk>
2015-10-08 14:36:04 +0200

    Issue #INNOVATION-89 by magnify: Tweaking for Drupal markup

--- innovationscenteret/_menus.php
+++ innovationscenteret/_menus.php
@@ -6,11 +6,12 @@
   <!--menu.html.twig START-->
   <nav class="nav js-nav">
     <div class="nav--links">
-      <a href="ydelser.php" class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">Ydelser</a>
-      <a href="om-os.php" class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">Om os</a>
-      <a href="nyheder.php" class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>">Nyheder</a>
-      <a href="kontakt.php" class="nav--link<?php echo $is_active === 3 ? ' is-active' : '' ?>">Kontakt</a>
-      <a href="designguide.php" class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>">Design guide</a>
+      <span class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>"><a href="ydelser.php">Ydelser</a></span>
+      <span class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>"><a href="om-os.php">Om os</a></span>
+      <span class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>"><a href="nyheder.php">Nyheder</a></span>
+      <span class="nav--link<?php echo $is_active === 3 ? ' is-active' : '' ?>"><a href="kontakt.php">Kontakt</a></span>
+      <span class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>"><a href="designguide.php">Design
+          guide</a></span>
     </div>
   </nav>
   <!--menu.html.twig END-->
@@ -23,11 +24,11 @@
   <!--menu.html.twig START-->
   <nav class="hamburger-menu">
     <div class="hamburger-menu--links">
-      <a href="ydelser.php" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">Ydelser</a>
-      <a href="om-os.php" class="hamburger-menu--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">Om os</a>
-      <a href="nyheder.php" class="hamburger-menu--link<?php echo $is_active === 2 ? ' is-active' : '' ?>">Nyheder</a>
-      <a href="kontakt.php" class="hamburger-menu--link<?php echo $is_active === 3 ? ' is-active' : '' ?>">Kontakt</a>
-      <a href="designguide.php" class="hamburger-menu--link<?php echo $is_active === 4 ? ' is-active' : '' ?>">Design
+      <a href="ydelser.php" class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">Ydelser</a>
+      <a href="om-os.php" class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">Om os</a>
+      <a href="nyheder.php" class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>">Nyheder</a>
+      <a href="kontakt.php" class="nav--link<?php echo $is_active === 3 ? ' is-active' : '' ?>">Kontakt</a>
+      <a href="designguide.php" class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>">Design
         guide</a>
     </div>
   </nav>
