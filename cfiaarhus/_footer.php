20dfa6376862079a01c4661dc5b67469db1d6d54
Jakob Rindom <rindom@gmail.com>
2015-11-30 11:53:29 +0100

    Issue #SUPPORT 389 by rindom: Updated footer css for IE9 and added padding to content.

--- cfiaarhus/_footer.php
+++ cfiaarhus/_footer.php
@@ -2,6 +2,9 @@
 <footer class="footer">
   <div class="footer--content">
     <!--block.html.twig START-->
+    <div class="footer--aarhus">
+      <a href="http://aarhus.dk"><figure class="footer--logo"><img src="/cfiaarhus/images/AAK-02-venstre-neg.png"></figure></a>
+    </div>
     <div class="footer--mail">
       <p>Dokk1, Hack Kampmanns Plads 2 8000 Aarhus C - <a href="mailto:cfi@aarhus.dk">cfi@aarhus.dk</a></p>
     </div>
@@ -13,9 +16,7 @@
       <a href="#"><figure class="footer--icons"><img src="images/linkedin.png"></figure></a>
     </div>
     <!--block.html.twig END-->
-    <div class="footer--aarhus">
-      <a href="http://aarhus.dk"><figure class="footer--logo"><img src="/cfiaarhus/images/AAK-02-venstre-neg.png"></figure></a>
-    </div>
+
   </div>
 </footer>
 <!--region.html.twig END-->
