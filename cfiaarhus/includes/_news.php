42a221b3b5d3fb141366fb67833d10055817561a
vagrant <vagrant@design.vm>
2016-01-27 08:27:22 +0100

    Rindom: Changed date on news

--- cfiaarhus/includes/_news.php
+++ cfiaarhus/includes/_news.php
@@ -1,7 +1,12 @@
-<h3 class="itk-boxlist-news--header-text">Fredag den 2/10-2015</h3>
 <article class="itk-boxlist-news--item">
   <header>
     <h2 class="itk-boxlist-news--header"><a href="nyhed.php">This is the news!</a></h2>
+    <div class="itk-boxlist-news--meta">
+      <p class="itk-boxlist-news--meta-wrapper">
+        <span class="itk-boxlist-news--icon"><img src="includes/icons/calendar.svg"></span>
+        4. oktober 2015
+      </p>
+    </div>
     <p>
       Sed haec quis possit intrepidus aestimare tellus. Curabitur blandit tempus ardua ridiculus sed magna. Morbi
       odio eros, volutpat ut pharetra vitae, lobortis sed nibh.
@@ -10,10 +15,15 @@
   </header>
   <figure><a href="nyhed.php"><img src="https://unsplash.it/499/500/?random"></a></figure>
 </article>
-<h3 class="itk-boxlist-news--header-text">Torsdag den 1/10-2015</h3>
 <article class="itk-boxlist-news--item">
   <header>
     <h2 class="itk-boxlist-news--header"><a href="nyhed.php">This is the news!</a></h2>
+    <div class="itk-boxlist-news--meta">
+      <p class="itk-boxlist-news--meta-wrapper">
+        <span class="itk-boxlist-news--icon"><img src="includes/icons/calendar.svg"></span>
+        3. oktober 2015
+      </p>
+    </div>
     <p>
       Sed haec quis possit intrepidus aestimare tellus. Curabitur blandit tempus ardua ridiculus sed magna. Morbi
       odio eros, volutpat ut pharetra vitae, lobortis sed nibh.
@@ -25,6 +35,12 @@
 <article class="itk-boxlist-news--item">
   <header>
     <h2 class="itk-boxlist-news--header"><a href="nyhed.php">This is the news!</a></h2>
+    <div class="itk-boxlist-news--meta">
+      <p class="itk-boxlist-news--meta-wrapper">
+        <span class="itk-boxlist-news--icon"><img src="includes/icons/calendar.svg"></span>
+        2. oktober 2015
+      </p>
+    </div>
     <p>
       Sed haec quis possit intrepidus aestimare tellus. Curabitur blandit tempus ardua ridiculus sed magna. Morbi
       odio eros, volutpat ut pharetra vitae, lobortis sed nibh.
