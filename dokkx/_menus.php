6f0de8376655e0b339e8edc7f5ec563d1dc22704
Brian Jensen <hallo@magnify.dk>
2016-02-15 15:36:44 +0100

    #DOKKX-5 Changed nav link styling and fixed sub page and menu

--- dokkx/_menus.php
+++ dokkx/_menus.php
@@ -6,12 +6,11 @@
   <!--menu.html.twig START-->
   <nav class="nav js-nav">
     <div class="nav--links">
-      <a class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>" href="aktivitet.php">Aktivitet</a>
-      <a class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>" href="besoeg-dokkx/">Besøg
+      <a class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>" href="/dokkx/aktivitet/">Aktivitet</a>
+      <a class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>" href="/dokkx/besoeg-dokkx/">Besøg
         DokkX</a>
-      <a class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>" href="dokkx.php">DokkX</a>
-      <a class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>" href="kontakt.php">Kontakt</a>
-      <a class="nav--link<?php echo $is_active === 5 ? ' is-active' : '' ?>" href="designguide.php">Design guide</a>
+      <a class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>" href="/dokkx/dokkx.php">DokkX</a>
+      <a class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>" href="/dokkx/kontakt.php">Kontakt</a>
     </div>
   </nav>
   <!--menu.html.twig END-->
@@ -25,17 +24,19 @@
   <nav class="hamburger-menu">
     <div class="hamburger-menu--inner">
       <div class="hamburger-menu--links">
-        <a href="aktivitet.php" class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">Aktivitet</a>
-        <a class="nav--link is-sub <?php echo $is_sub_active === 0 ? ' is-active' : '' ?>" href="sundhed-traening.php">Sundhed og træning</a>
-        <a class="nav--link is-sub <?php echo $is_sub_active === 1 ? ' is-active' : '' ?>" href="teknologier.php">Teknologier</a>
-        <a class="nav--link is-sub <?php echo $is_sub_active === 2 ? ' is-active' : '' ?>" href="arkiv.php">Arkiv</a>
-        <a href="besoeg-dokkx/index.php" class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">Besøg
+        <a href="/dokkx/aktivitet/" class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">Aktivitet</a>
+        <a class="nav--link is-sub <?php echo $is_sub_active[0] === 0 ? ' is-active' : '' ?>"
+           href="/dokkx/sundhed-traening.php">Sundhed og træning</a>
+        <a class="nav--link is-sub <?php echo $is_sub_active[0] === 1 ? ' is-active' : '' ?>" href="/dokkx/teknologier.php">Teknologier</a>
+        <a class="nav--link is-sub <?php echo $is_sub_active[0] === 2 ? ' is-active' : '' ?>" href="/dokkx/arkiv.php">Arkiv</a>
+        <a href="/dokkx/besoeg-dokkx/" class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">Besøg
           DokkX</a>
-        <a class="nav--link is-sub <?php echo $is_sub_active === 0 ? ' is-active' : '' ?>" href="besoeg-dokkx/aabningstider.php">Åbningstider</a>
-        <a href="nyheder.php" class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>">Nyheder</a>
-        <a href="events.php" class="nav--link<?php echo $is_active === 3 ? ' is-active' : '' ?>">Events</a>
-        <a href="kontakt.php" class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>">Kontakt</a>
-        <a href="designguide.php" class="nav--link<?php echo $is_active === 5 ? ' is-active' : '' ?>">Design
+        <a class="nav--link is-sub <?php echo $is_sub_active[1] === 0 ? ' is-active' : '' ?>"
+           href="/dokkx/besoeg-dokkx/aabningstider.php">Åbningstider</a>
+        <a href="/dokkx/nyheder.php" class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>">Nyheder</a>
+        <a href="/dokkx/events.php" class="nav--link<?php echo $is_active === 3 ? ' is-active' : '' ?>">Events</a>
+        <a href="/dokkx/kontakt.php" class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>">Kontakt</a>
+        <a href="/dokkx/designguide.php" class="nav--link<?php echo $is_active === 5 ? ' is-active' : '' ?>">Design
           guide</a>
       </div>
     </div>
