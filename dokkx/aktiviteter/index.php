79a1c3caa343f84e46f36b3ba7925d715d334a4b
Brian Jensen <hallo@magnify.dk>
2016-02-18 15:33:03 +0100

    #DOKKX-5 Updated content fixed minor design issues

--- dokkx/aktiviteter/index.php
+++ dokkx/aktiviteter/index.php
@@ -11,38 +11,24 @@
 <main role="main" class="content">
   <h1 class="content--header">Aktiviteter</h1>
   <div class="content--lead">
-    <p>At nos hinc posthac, sitientis piros Afros. Ut enim ad minim veniam, quis nostrud exercitation. Ambitioni dedisse scripsisse iudicaretur. Prima luce, cum quibus mons aliud  consensu ab eo.</p>
+    <p>[Mangler tekst]</p>
   </div>
   <div class="content--text">
-    <p>Salutantibus vitae elit libero, a pharetra augue. Quam diu etiam furor iste tuus nos eludet? Non equidem invideo, miror magis posuere velit aliquet. Quisque placerat facilisis egestas cillum dolore.</p>
-    <p>Hi omnes lingua, institutis, legibus inter se differunt. Quid securi etiam tamquam eu fugiat nulla pariatur. A communi observantia non est recedendum.</p>
-    <p>Quam temere in vitiis, legem sancimus haerentia. Integer legentibus erat a ante historiarum dapibus. Ab illo tempore, ab est sed immemorabili.</p>
+    <p>[Mangler tekst]</p>
   </div>
   <div class="itk-boxlist">
     <article class="itk-boxlist--item">
       <header>
         <h2 class="itk-boxlist--header"><a href="/dokkx/aktiviteter/sundhed-traening.php">Sundhed og træning</a></h2>
-        <p>Der findes utallige teknologier, du kan bruge til at holde dig sund og i form gennem hele livet. Sammen med en bid brain food vil gæsterne blive budt på teknologi til alt fra selvmonitorering, altså, fx skridttællere og søvnmålere i apps, mobiltelefoner og ure, over Interaktive spil for både unge og gamle til teknologi til kognitiv og fysisk fitness.
+        <p>
+          Der findes utallige teknologier, du kan bruge til at holde dig sund og i form gennem hele livet. Sammen med
+          en bid brain food vil gæsterne blive budt på teknologi til alt fra selvmonitorering, altså, fx skridttællere
+          og søvnmålere i apps, mobiltelefoner og ure, over Interaktive spil for både unge og gamle til teknologi til
+          kognitiv og fysisk fitness.
         </p>
         <a href="/dokkx/aktiviteter/sundhed-traening.php">Sundhed og træning</a>
       </header>
-      <figure><a href="netvaerk.php"><img src="https://unsplash.it/500/425/?random"></a></figure>
-    </article>
-    <article class="itk-boxlist--item">
-      <header>
-        <h2 id="network" class="itk-boxlist--header"><a href="prototyper.php">Prototyper</a></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Hi omnes lingua, institutis, legibus inter se differunt. Cum ceteris in veneratione tui montes, nascetur mus.</p>
-        <a href="prototyper.php">Læs om prototyper</a>
-      </header>
-      <figure><a href="netvaerk.php"><img src="https://unsplash.it/500/450/?random"></a></figure>
-    </article>
-    <article class="itk-boxlist--item">
-      <header>
-        <h2 class="itk-boxlist--header"><a href="sparring.php">Sparring</a></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Hi omnes lingua, institutis, legibus inter se differunt. Cum ceteris in veneratione tui montes, nascetur mus.</p>
-        <a href="sparring.php">Læs om sparring</a>
-      </header>
-      <figure><a href="netvaerk.php"><img src="https://unsplash.it/500/475/?random"></a></figure>
+      <figure><a href="/dokkx/aktiviteter/sundhed-traening.php"><img src="https://unsplash.it/500/425/?random"></a></figure>
     </article>
   </div>
 </main>
