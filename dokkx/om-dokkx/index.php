d171d7da4891d342b727ac0a5e5f6000d84f0912
Brian Jensen <hallo@magnify.dk>
2016-02-24 11:08:29 +0100

    #DOKKX-5  Added content

--- dokkx/om-dokkx/index.php
+++ dokkx/om-dokkx/index.php
@@ -15,44 +15,65 @@
   </div>
   <div class="content--text">
     <p>
-      DokkX er Aarhus Kommunes nytænkende udviklingscenter for velfærdsteknologi og andre digitale løsninger.
-      Det er her, du kan du se nye teknologiske løsninger og prøve dem af. Vores beskedne ønske er, at du går fra DokkX
-      med ny inspiration til, hvordan teknologi kan gøre din hverdag lettere, sundere og sjovere. Intet mindre.
-    </p>
-    <p>
       Vi ved, at teknologi i forvejen er en del af mange menneskers hverdag – uanset og vi tænker over det eller ej.
       Ved at skabe et rum på Dokk1, hvor alle kan føle, røre og lege med forskellige teknologiske løsninger, skal DokkX
       give inspiration til og viden om, hvordan teknologi gennem hele tilværelsen kan være et redskab til et lidt bedre,
       lidt sundere og lidt mere socialt liv. Det kan være teknologi til både krop og sind, og det kan være til at skabe
       et mere tilgængeligt, bæredygtigt og trygt byliv for os alle sammen.
     </p>
+    <p>
+      <strong>Frihed til mere til flere</strong><br>
+      Mange mennesker får allerede flere muligheder og mere frihed i hverdagen med teknologi
+      Vi ved, at teknologi i forvejen er en del af mange menneskers hverdag – uanset og vi tænker over det eller ej.
+      Aarhus Kommune har i flere år arbejdet med velfærdsteknologi. Især på ældre- og handicapområdet har teknologien
+      givet mange mennesker muligheder og en frihed i hverdagen, de ellers ville være afskåret fra. Nu breder de gode
+      løsninger sig, i takt med at teknologi bliver stadig billigere, smartere og lettere at gå til. Ved at skabe et rum
+      på Dokk1, hvor alle kan føle, røre og lege med forskellige teknologiske løsninger, skal DokkX give inspiration til
+      og viden om, hvordan teknologi gennem hele tilværelsen kan være et redskab til et lidt bedre, lidt sundere og lidt
+      mere socialt liv. Det kan være teknologi til både krop og sind, og det kan være til at skabe et mere tilgængeligt,
+      bæredygtigt og trygt byliv for os alle sammen.
+    </p>
+    <p>
+      <a href="http://www.aarhus.dk/sitecore/content/Subsites/Velfaerdsteknologi/Home.aspx">Læs mere om Aarhus Kommunes
+        arbejde med velfærdsteknologi</a>
+    </p>
+    <p>
+      <strong>Rum for gentænking<br>
+      Et innovativt miljø for afsmittende samtaler og samarbejde</strong><br>
+      Ud over at vise moderne velfærdsteknologi er DokkX et unikt samlingspunkt, hvor der kan opstå nye samarbejder mellem erhvervsliv, uddannelses- og forskningsinstitutioner, Aarhus Kommune og byens borgere.
+      Når DokkX ønsker at rumme og samle forskellige grupper, fagligheder, synspunkter fra og perspektiver på tilværelsen. Det gør vi med en ambition om, at der opblomstrer et innovativt miljø for afsmittende samtaler og samarbejde om alt fra samfundsforskning til udvikling af fremtidens teknologiske løsninger.
+    </p>
+    <p>
+      <a href="#">Se, hvem der allerede arbejder sammen om DokkX</a>
+    </p>
+    <p>
+      <strong>Centrum for velfærds- og Frihedsteknologi<br>
+      Pulserende, dynamisk og afvekslende teknologisk prøverum</strong><br>
+      DokkX adskiller sig fra andre udstillings- og demonstrationsfaciliteter rundt omkring i Danmark på en række punkter. Først og fremmest ligger vi lige midt i byen. Vores centrale og lettilgængelige placering på Dokk1 giver de fleste mulighed for at besøge os. Det er praktisk, for vi tror, at der på DokkX er noget at komme efter for alle. Udstillingerne på DokkX er afvekslende og dynamiske. I samarbejde med brugerorganisationer, producenter og andre interessenter sætter vi løbende fokus på forskellige temaer fra tilværelsen.
+    </p>
+    <p>
+      <strong>Facts</strong><br>
+      Placering: Dokk1, Hack Kampmanns Plads 2, 8000 Aarhus C<br>
+      Størrelse: 250 m2<br>
+      Åbning: Forår 2016
+    </p>
   </div>
   <div class="itk-boxlist">
     <article class="itk-boxlist--item">
       <header>
-        <h2 class="itk-boxlist--header"><a href="netvaerk.php">Netværk</a></h2>
-        <p>Til dig der gerne vil dele din erfaring og inspireres af andre kollegaer og eksperter tilbyder Center for
-        Innovation i Aarhus deltagelse i vores innovationsnetværk. Her møder du kollegaer i Aarhus Kommune, der
-        arbejder med innovation ligesom dig.</p>
-        <a href="netvaerk.php">Læs om netværk</a>
+        <h2 class="itk-boxlist--header"><a href="/dokkx/aktiviteter/">Aktuelt på DokkX</a></h2>
+        <p>[Mangler tekst]</p>
+        <a href="/dokkx/aktiviteter/">[Mangler tekst]</a>
       </header>
-      <figure><a href="netvaerk.php"><img src="images/networking.png"></a></figure>
+      <figure><a href="/dokkx/aktiviteter/"><img src="https://unsplash.it/500/500/?random"</a></figure>
     </article>
     <article class="itk-boxlist--item">
       <header>
-        <h2 id="network" class="itk-boxlist--header"><a href="prototyper.php">Prototyper</a></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Hi omnes lingua, institutis, legibus inter se differunt. Cum ceteris in veneratione tui montes, nascetur mus.</p>
-        <a href="prototyper.php">Læs om prototyper</a>
+        <h2 class="itk-boxlist--header"><a href="#">Find vej</a></h2>
+        <p>[Mangler tekst]</p>
+        <a href="#">[Mangler tekst]</a>
       </header>
-      <figure><a href="prototyper.php"><img src="images/prototypes.png"></a></figure>
-    </article>
-    <article class="itk-boxlist--item">
-      <header>
-        <h2 class="itk-boxlist--header"><a href="sparring.php">Sparring</a></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Hi omnes lingua, institutis, legibus inter se differunt. Cum ceteris in veneratione tui montes, nascetur mus.</p>
-        <a href="sparring.php">Læs om sparring</a>
-      </header>
-      <figure><a href="sparring.php"><img src="images/sparring.png"></a></figure>
+      <figure><a href="#"><img src="https://unsplash.it/500/501/?random"</a></figure>
     </article>
   </div>
 </main>
