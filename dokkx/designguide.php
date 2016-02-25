bb7f19db653a359949c1c6e2d7465ed3a291ead8
Brian Jensen <hallo@magnify.dk>
2016-02-25 15:01:15 +0100

    #DOKKX-28 Updated designguide and added more form examples

--- dokkx/designguide.php
+++ dokkx/designguide.php
@@ -66,13 +66,68 @@
   <fieldset>
     <legend>Forms</legend>
     <div class="content-form">
+      <h3>Text field header 3</h3>
       <div class="content-form--item">
-        <label for="name">Label</label>
-        <input id="name" type="text">
+        <label for="text-1">Text field 1</label>
+        <input id="text-1" type="text">
       </div>
       <div class="content-form--item">
-        <label for="name">Disabled</label>
+        <label for="text-2">Text field 2</label>
+        <input id="text-2" type="text">
+      </div>
+      <h4>Text field header 4</h4>
+      <div class="content-form--item">
+        <label for="text-1">Text field 1</label>
+        <input id="text-1" type="text">
+      </div>
+      <div class="content-form--item">
+        <label for="text-2">Text field 2</label>
+        <input id="text-2" type="text">
+      </div>
+      <div class="content-form--item">
+        <label for="name">Disabled text field</label>
         <input id="name" type="text" disabled>
+      </div>
+      <div class="content-form--item is-radio">
+        <h4>Radio button list</h4>
+        <label>
+          <input type="radio" name="radio">
+          Radio button 1
+        </label>
+        <label>
+          <input type="radio" name="radio">
+          Radio button 2
+        </label>
+        <label>
+          <input type="radio" name="radio">
+          Radio button 3
+        </label>
+      </div>
+      <h4>Checkbox list</h4>
+      <div class="content-form--item is-checkbox">
+        <label>
+          <input type="checkbox" name="checkbox-1">
+          Checkbox 1
+        </label>
+        <label>
+          <input type="checkbox" name="checkbox-2">
+          Checkbox 2
+        </label>
+        <label>
+          <input type="checkbox" name="checkbox-3">
+          Checkbox 3
+        </label>
+      </div>
+      <h4>Disabled checkbox</h4>
+      <div class="content-form--item is-checkbox">
+        <label>
+          <input type="checkbox" name="checkbox-1" disabled>
+          You can't check this
+        </label>
+      </div>
+      <div class="content-form--item">
+        <h4>Textarea</h4>
+        <textarea>This is a textarea.</textarea>
       </div>
       <a href="#" class="button">This is the button for the form</a>
     </div>
