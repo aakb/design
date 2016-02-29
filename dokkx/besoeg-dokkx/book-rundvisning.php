aa00ed1e93af963aadfa2398821f3f7939e041d4
Brian Jensen <hallo@magnify.dk>
2016-02-29 10:38:43 +0100

    #DOKKX-28 Added content inner wrapper and fixed some polygon styling

--- dokkx/besoeg-dokkx/book-rundvisning.php
+++ dokkx/besoeg-dokkx/book-rundvisning.php
@@ -7,41 +7,43 @@
   include '../_menus.php';
 ?>
 
-<?php include '../includes/_image.php'; ?>
 <main role="main" class="content">
-  <h1 class="content--header">Book en rundvisning</h1>
-  <div class="content--lead">
-    <p>[Mangler tekst]</p>
+  <?php include '../includes/_image.php'; ?>
+  <div class="content--inner">
+    <h1 class="content--header">Book en rundvisning</h1>
+    <div class="content--lead">
+      <p>[Mangler tekst]</p>
+    </div>
+    <div class="content--text">
+      <p>
+        Alle er velkomne på DokkX i vores åbningstider. Du kan frit boltre dig blandt vores spændende teknologier, og her
+        er altid en medarbejder, du kan spørge til råds om stort og småt.
+      </p>
+      <p>
+        Hvis du kommer fra en anden kommune, en organisation eller en forening kan du booke en rundvisning på DokkX.
+        Så skriv ”Book rundvisning” i emnefeltet og send os en MAIL. Eller ring og hør nærmere på TLF.
+      </p>
+    </div>
+    <fieldset>
+      <legend>Book en rundvisning</legend>
+      <p>Udfyld formularen for at booke en rundvisning, vi skal bruge dit navn og e-mail adresse for at kontakte dig.</p>
+      <form class="content-form" id="#form" name="#form">
+        <div class="content-form--item">
+          <label for="name">Navn</label>
+          <input id="name" type="text" placeholder="Indtast dit fulde navn" required>
+        </div>
+        <div class="content-form--item">
+          <label for="email">E-mail</label>
+          <input id="email" type="email" placeholder="Indtast e-mail adresse" required>
+        </div>
+        <div class="content-form--item">
+          <label for="message">Evt. besked</label>
+          <textarea id="message" placeholder="Skriv evt. en besked"></textarea>
+        </div>
+        <a href="book-rundvisning-form-error.php" class="button">Send forespørgsel</a>
+      </form>
+    </fieldset>
+    <?php include $path . '/includes/_instagram.php'; ?>
   </div>
-  <div class="content--text">
-    <p>
-      Alle er velkomne på DokkX i vores åbningstider. Du kan frit boltre dig blandt vores spændende teknologier, og her
-      er altid en medarbejder, du kan spørge til råds om stort og småt.
-    </p>
-    <p>
-      Hvis du kommer fra en anden kommune, en organisation eller en forening kan du booke en rundvisning på DokkX.
-      Så skriv ”Book rundvisning” i emnefeltet og send os en MAIL. Eller ring og hør nærmere på TLF.
-    </p>
-  </div>
-  <fieldset>
-    <legend>Book en rundvisning</legend>
-    <p>Udfyld formularen for at booke en rundvisning, vi skal bruge dit navn og e-mail adresse for at kontakte dig.</p>
-    <form class="content-form" id="#form" name="#form">
-      <div class="content-form--item">
-        <label for="name">Navn</label>
-        <input id="name" type="text" placeholder="Indtast dit fulde navn" required>
-      </div>
-      <div class="content-form--item">
-        <label for="email">E-mail</label>
-        <input id="email" type="email" placeholder="Indtast e-mail adresse" required>
-      </div>
-      <div class="content-form--item">
-        <label for="message">Evt. besked</label>
-        <textarea id="message" placeholder="Skriv evt. en besked"></textarea>
-      </div>
-      <a href="book-rundvisning-form-error.php" class="button">Send forespørgsel</a>
-    </form>
-  </fieldset>
-  <?php include $path . '/includes/_instagram.php'; ?>
 </main>
 <?php include $path . '/_footer.php'; ?>
--- dokkx/om-dokkx/index.php
+++ dokkx/om-dokkx/index.php
@@ -7,83 +7,85 @@
   include '../_menus.php';
 ?>
 
-<?php include '../includes/_image.php'; ?>
 <main role="main" class="content">
-  <h1 class="content--header">Om DokkX</h1>
-  <div class="content--lead">
-    <p>Centrum for velfærds- og frihedsteknologi</p>
-  </div>
-  <div class="content--text">
-    <p>
-      Vi ved, at teknologi i forvejen er en del af mange menneskers hverdag – uanset og vi tænker over det eller ej.
-      Ved at skabe et rum på Dokk1, hvor alle kan føle, røre og lege med forskellige teknologiske løsninger, skal DokkX
-      give inspiration til og viden om, hvordan teknologi gennem hele tilværelsen kan være et redskab til et lidt bedre,
-      lidt sundere og lidt mere socialt liv. Det kan være teknologi til både krop og sind, og det kan være til at skabe
-      et mere tilgængeligt, bæredygtigt og trygt byliv for os alle sammen.
-    </p>
-    <h3>Frihed til mere til flere</h3>
-    <h4>Mange mennesker får allerede flere muligheder og mere frihed i hverdagen med teknologi</h4>
-    <p>
-      Vi ved, at teknologi i forvejen er en del af mange menneskers hverdag – uanset og vi tænker over det eller ej.
-      Aarhus Kommune har i flere år arbejdet med velfærdsteknologi. Især på ældre- og handicapområdet har teknologien
-      givet mange mennesker muligheder og en frihed i hverdagen, de ellers ville være afskåret fra. Nu breder de gode
-      løsninger sig, i takt med at teknologi bliver stadig billigere, smartere og lettere at gå til. Ved at skabe et rum
-      på Dokk1, hvor alle kan føle, røre og lege med forskellige teknologiske løsninger, skal DokkX give inspiration til
-      og viden om, hvordan teknologi gennem hele tilværelsen kan være et redskab til et lidt bedre, lidt sundere og lidt
-      mere socialt liv. Det kan være teknologi til både krop og sind, og det kan være til at skabe et mere tilgængeligt,
-      bæredygtigt og trygt byliv for os alle sammen.
-    </p>
-    <p>
-      Vi ved, at teknologi i forvejen er en del af mange menneskers hverdag – uanset og vi tænker over det eller ej.
-      Aarhus Kommune har i flere år arbejdet med velfærdsteknologi. Især på ældre- og handicapområdet har teknologien
-      givet mange mennesker muligheder og en frihed i hverdagen, de ellers ville være afskåret fra. Nu breder de gode
-      løsninger sig, i takt med at teknologi bliver stadig billigere, smartere og lettere at gå til. Ved at skabe et rum
-      på Dokk1, hvor alle kan føle, røre og lege med forskellige teknologiske løsninger, skal DokkX give inspiration til
-      og viden om, hvordan teknologi gennem hele tilværelsen kan være et redskab til et lidt bedre, lidt sundere og lidt
-      mere socialt liv. Det kan være teknologi til både krop og sind, og det kan være til at skabe et mere tilgængeligt,
-      bæredygtigt og trygt byliv for os alle sammen.
-    </p>
-    <p class="content--inline-link"><a href="http://www.aarhus.dk/sitecore/content/Subsites/Velfaerdsteknologi/Home.aspx">Læs mere om Aarhus Kommunes
-      arbejde med velfærdsteknologi</a>
-    </p>
-    <h3>Rum for gentænking</h3>
-    <h4>Et innovativt miljø for afsmittende samtaler og samarbejde</h4>
-    <p>
-      Ud over at vise moderne velfærdsteknologi er DokkX et unikt samlingspunkt, hvor der kan opstå nye samarbejder mellem erhvervsliv, uddannelses- og forskningsinstitutioner, Aarhus Kommune og byens borgere.
-      Når DokkX ønsker at rumme og samle forskellige grupper, fagligheder, synspunkter fra og perspektiver på tilværelsen. Det gør vi med en ambition om, at der opblomstrer et innovativt miljø for afsmittende samtaler og samarbejde om alt fra samfundsforskning til udvikling af fremtidens teknologiske løsninger.
-    </p>
-    <p class="content--inline-link">
-      <a href="#">Se, hvem der allerede arbejder sammen om DokkX</a>
-    </p>
-    <h3>Centrum for velfærds- og Frihedsteknologi</h3>
-    <h4>Pulserende, dynamisk og afvekslende teknologisk prøverum</h4>
-    <p>
-      DokkX adskiller sig fra andre udstillings- og demonstrationsfaciliteter rundt omkring i Danmark på en række punkter. Først og fremmest ligger vi lige midt i byen. Vores centrale og lettilgængelige placering på Dokk1 giver de fleste mulighed for at besøge os. Det er praktisk, for vi tror, at der på DokkX er noget at komme efter for alle. Udstillingerne på DokkX er afvekslende og dynamiske. I samarbejde med brugerorganisationer, producenter og andre interessenter sætter vi løbende fokus på forskellige temaer fra tilværelsen.
-    </p>
-    <h3>Facts</h3>
-    <p>
-      Placering: Dokk1, Hack Kampmanns Plads 2, 8000 Aarhus C<br>
-      Størrelse: 250 m2<br>
-      Åbning: Forår 2016
-    </p>
-  </div>
-  <div class="itk-boxlist">
-    <article class="itk-boxlist--item">
-      <header>
-        <h2 class="itk-boxlist--header"><a href="/dokkx/aktiviteter/">Aktuelt på DokkX</a></h2>
-        <p>[Mangler tekst]</p>
-        <a href="#">[Mangler tekst]</a>
-      </header>
-      <figure><a href="#"><img src="https://unsplash.it/500/501/?random"></a></figure>
-    </article>
-    <article class="itk-boxlist--item">
-      <header>
-        <h2 class="itk-boxlist--header"><a href="#">Find vej</a></h2>
-        <p>[Mangler tekst]</p>
-        <a href="#">[Mangler tekst]</a>
-      </header>
-      <figure><a href="#"><img src="https://unsplash.it/500/501/?random"></a></figure>
-    </article>
+  <?php include '../includes/_image.php'; ?>
+  <div class="content--inner">
+    <h1 class="content--header">Om DokkX</h1>
+    <div class="content--lead">
+      <p>Centrum for velfærds- og frihedsteknologi</p>
+    </div>
+    <div class="content--text">
+      <p>
+        Vi ved, at teknologi i forvejen er en del af mange menneskers hverdag – uanset og vi tænker over det eller ej.
+        Ved at skabe et rum på Dokk1, hvor alle kan føle, røre og lege med forskellige teknologiske løsninger, skal DokkX
+        give inspiration til og viden om, hvordan teknologi gennem hele tilværelsen kan være et redskab til et lidt bedre,
+        lidt sundere og lidt mere socialt liv. Det kan være teknologi til både krop og sind, og det kan være til at skabe
+        et mere tilgængeligt, bæredygtigt og trygt byliv for os alle sammen.
+      </p>
+      <h3>Frihed til mere til flere</h3>
+      <h4>Mange mennesker får allerede flere muligheder og mere frihed i hverdagen med teknologi</h4>
+      <p>
+        Vi ved, at teknologi i forvejen er en del af mange menneskers hverdag – uanset og vi tænker over det eller ej.
+        Aarhus Kommune har i flere år arbejdet med velfærdsteknologi. Især på ældre- og handicapområdet har teknologien
+        givet mange mennesker muligheder og en frihed i hverdagen, de ellers ville være afskåret fra. Nu breder de gode
+        løsninger sig, i takt med at teknologi bliver stadig billigere, smartere og lettere at gå til. Ved at skabe et rum
+        på Dokk1, hvor alle kan føle, røre og lege med forskellige teknologiske løsninger, skal DokkX give inspiration til
+        og viden om, hvordan teknologi gennem hele tilværelsen kan være et redskab til et lidt bedre, lidt sundere og lidt
+        mere socialt liv. Det kan være teknologi til både krop og sind, og det kan være til at skabe et mere tilgængeligt,
+        bæredygtigt og trygt byliv for os alle sammen.
+      </p>
+      <p>
+        Vi ved, at teknologi i forvejen er en del af mange menneskers hverdag – uanset og vi tænker over det eller ej.
+        Aarhus Kommune har i flere år arbejdet med velfærdsteknologi. Især på ældre- og handicapområdet har teknologien
+        givet mange mennesker muligheder og en frihed i hverdagen, de ellers ville være afskåret fra. Nu breder de gode
+        løsninger sig, i takt med at teknologi bliver stadig billigere, smartere og lettere at gå til. Ved at skabe et rum
+        på Dokk1, hvor alle kan føle, røre og lege med forskellige teknologiske løsninger, skal DokkX give inspiration til
+        og viden om, hvordan teknologi gennem hele tilværelsen kan være et redskab til et lidt bedre, lidt sundere og lidt
+        mere socialt liv. Det kan være teknologi til både krop og sind, og det kan være til at skabe et mere tilgængeligt,
+        bæredygtigt og trygt byliv for os alle sammen.
+      </p>
+      <p class="content--inline-link"><a href="http://www.aarhus.dk/sitecore/content/Subsites/Velfaerdsteknologi/Home.aspx">Læs mere om Aarhus Kommunes
+          arbejde med velfærdsteknologi</a>
+      </p>
+      <h3>Rum for gentænking</h3>
+      <h4>Et innovativt miljø for afsmittende samtaler og samarbejde</h4>
+      <p>
+        Ud over at vise moderne velfærdsteknologi er DokkX et unikt samlingspunkt, hvor der kan opstå nye samarbejder mellem erhvervsliv, uddannelses- og forskningsinstitutioner, Aarhus Kommune og byens borgere.
+        Når DokkX ønsker at rumme og samle forskellige grupper, fagligheder, synspunkter fra og perspektiver på tilværelsen. Det gør vi med en ambition om, at der opblomstrer et innovativt miljø for afsmittende samtaler og samarbejde om alt fra samfundsforskning til udvikling af fremtidens teknologiske løsninger.
+      </p>
+      <p class="content--inline-link">
+        <a href="#">Se, hvem der allerede arbejder sammen om DokkX</a>
+      </p>
+      <h3>Centrum for velfærds- og Frihedsteknologi</h3>
+      <h4>Pulserende, dynamisk og afvekslende teknologisk prøverum</h4>
+      <p>
+        DokkX adskiller sig fra andre udstillings- og demonstrationsfaciliteter rundt omkring i Danmark på en række punkter. Først og fremmest ligger vi lige midt i byen. Vores centrale og lettilgængelige placering på Dokk1 giver de fleste mulighed for at besøge os. Det er praktisk, for vi tror, at der på DokkX er noget at komme efter for alle. Udstillingerne på DokkX er afvekslende og dynamiske. I samarbejde med brugerorganisationer, producenter og andre interessenter sætter vi løbende fokus på forskellige temaer fra tilværelsen.
+      </p>
+      <h3>Facts</h3>
+      <p>
+        Placering: Dokk1, Hack Kampmanns Plads 2, 8000 Aarhus C<br>
+        Størrelse: 250 m2<br>
+        Åbning: Forår 2016
+      </p>
+    </div>
+    <div class="itk-boxlist">
+      <article class="itk-boxlist--item">
+        <header>
+          <h2 class="itk-boxlist--header"><a href="/dokkx/aktiviteter/">Aktuelt på DokkX</a></h2>
+          <p>[Mangler tekst]</p>
+          <a href="#">[Mangler tekst]</a>
+        </header>
+        <figure><a href="#"><img src="https://unsplash.it/500/501/?random"></a></figure>
+      </article>
+      <article class="itk-boxlist--item">
+        <header>
+          <h2 class="itk-boxlist--header"><a href="#">Find vej</a></h2>
+          <p>[Mangler tekst]</p>
+          <a href="#">[Mangler tekst]</a>
+        </header>
+        <figure><a href="#"><img src="https://unsplash.it/500/501/?random"></a></figure>
+      </article>
+    </div>
   </div>
 </main>
 <?php include $path . '/_footer.php'; ?>
