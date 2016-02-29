56b08ad5d9e9394d15d92294dbd7e1e6558fec44
Brian Jensen <hallo@magnify.dk>
2016-02-29 10:41:19 +0100

    #DOKKX-32 Fixed link

--- dokkx/aktiviteter/index.php
+++ dokkx/aktiviteter/index.php
@@ -33,15 +33,15 @@
       </article>
       <article class="itk-boxlist--item">
         <header>
-          <h2 class="itk-boxlist--header"><a href="/dokkx/aktiviteter/book-rundvisning.php">Book en rundvisning</a></h2>
+          <h2 class="itk-boxlist--header"><a href="/dokkx/besoeg-dokkx/book-rundvisning.php">Book en rundvisning</a></h2>
           <p>
             Alle er velkomne på DokkX i vores åbningstider. Du kan frit boltre dig blandt vores spændende teknologier, og
             her er altid en medarbejder, du kan spørge til råds om stort og småt.
           </p>
-          <a href="/dokkx/aktiviteter/book-rundvisning.php">Book en rundvisning</a>
+          <a href="/dokkx/besoeg-dokkx/book-rundvisning.php">Book en rundvisning</a>
         </header>
         <figure><a href="/dokkx/aktiviteter/book-rundvisning.php"><img src="https://unsplash.it/500/400/?random"></a></figure>
-      </article>
+        besoeg-dokkx
     </div>
   </div>
 </main>
