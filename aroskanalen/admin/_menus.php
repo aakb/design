ebed5750a3570b20ded5b913f3be5b83c4c6f835
Brian Jensen <hallo@magnify.dk>
2016-03-18 13:10:26 +0100

    #ARU-37 Design

--- aroskanalen/admin/_menus.php
+++ aroskanalen/admin/_menus.php
@@ -1,5 +1,24 @@
 <body class="<?php echo $class; ?>">
   <div class="overlay js-menu-toggle"></div>
+  <header class="header">
+    <div class="header--inner">
+      <a href="<?php echo $path_to_site; ?>" class="logo js-logo" title="Aroskanalen"><span>Aroskanalen</span></a>
+      <nav class="nav js-nav">
+          <a class="nav--link<?php echo $is_active === 0 || $is_sub_active[0] ? ' is-active' : '' ?>" href="#">
+            Slides
+          </a>
+          <a class="nav--link<?php echo $is_active === 1 || $is_sub_active[1] ? ' is-active' : '' ?>" href="#">
+            Kanaler
+          </a>
+          <a class="nav--link<?php echo $is_active === 2 || $is_sub_active[2] ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/screen/">
+            Skærme
+          </a>
+        <a class="nav--link<?php echo $is_active === 3 || $is_sub_active[3] ? ' is-active' : '' ?>" href="#">
+          Medier
+        </a>
+      </nav>
+    </div>
+  </header>
   <div class="nav-toggle js-menu-toggle">
     <span></span>
     <span></span>
@@ -13,15 +32,47 @@
           <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
             Kanaler
           </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[0] === 1 ? ' is-active' : '' ?>" href="#">
+            Oversigt
+          </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[1] === 1 ? ' is-active' : '' ?>" href="#">
+            Opret kanal
+          </a>
         </div>
         <div class="hamburger-menu--group">
           <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
             Slides
           </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[0] === 1 ? ' is-active' : '' ?>" href="#">
+            Oversigt
+          </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[1] === 1 ? ' is-active' : '' ?>" href="#">
+            Opret kanal
+          </a>
         </div>
         <div class="hamburger-menu--group">
           <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
             Skærme
+          </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[0] === 1 ? ' is-active' : '' ?>" href="#">
+            Oversigt
+          </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[1] === 1 ? ' is-active' : '' ?>" href="#">
+            Opret kanal
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
+            Administration
+          </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[0] === 1 ? ' is-active' : '' ?>" href="#">
+            Deling
+          </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[1] === 1 ? ' is-active' : '' ?>" href="#">
+            Templates
+          </a>
+          <a class="hamburger-menu--link is-sub <?php echo $is_sub_active[1] === 1 ? ' is-active' : '' ?>" href="#">
+            Søgning og indhold
           </a>
         </div>
         <div class="hamburger-menu--group">
@@ -32,22 +83,3 @@
       </div>
     </div>
   </nav>
-  <header class="header">
-    <div class="header--inner">
-      <a href="<?php echo $path_to_site; ?>" class="logo js-logo" title="Aroskanalen"><span>Aroskanalen</span></a>
-      <nav class="nav js-nav">
-          <a class="nav--link<?php echo $is_active === 0 || $is_sub_active[0] ? ' is-active' : '' ?>" href="#">
-            Slides
-          </a>
-          <a class="nav--link<?php echo $is_active === 1 || $is_sub_active[1] ? ' is-active' : '' ?>" href="#">
-            Kanaler
-          </a>
-          <a class="nav--link<?php echo $is_active === 2 || $is_sub_active[2] ? ' is-active' : '' ?>" href="#">
-            Skærme
-          </a>
-        <a class="nav--link<?php echo $is_active === 3 || $is_sub_active[3] ? ' is-active' : '' ?>" href="#">
-          Medier
-        </a>
-      </nav>
-    </div>
-  </header>
