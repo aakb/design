21071a0fbfc8254868e320b109ca45ac2521bfb2
Brian Jensen <hallo@magnify.dk>
2016-03-18 11:25:15 +0100

    #ARU-37 Added new font, added color and missing javascripts

--- aroskanalen/admin/_menus.php
+++ aroskanalen/admin/_menus.php
@@ -1,5 +1,37 @@
 <body class="<?php echo $class; ?>">
   <div class="overlay js-menu-toggle"></div>
+  <div class="nav-toggle js-menu-toggle">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  <nav class="hamburger-menu">
+    <div class="hamburger-menu--inner">
+      <div class="hamburger-menu--links">
+        <div class="hamburger-menu--group">
+          <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
+            Kanaler
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
+            Slides
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
+            Skærme
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="/aroskanalen/style-guide" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
+            Style guide
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
   <header class="header">
     <div class="header--inner">
       <a href="<?php echo $path_to_site; ?>" class="logo js-logo" title="Aroskanalen"><span>Aroskanalen</span></a>
@@ -16,38 +48,6 @@
         <a class="nav--link<?php echo $is_active === 3 || $is_sub_active[3] ? ' is-active' : '' ?>" href="#">
           Medier
         </a>
-      </nav>
-      <div class="nav-toggle js-menu-toggle">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <nav class="hamburger-menu">
-        <div class="hamburger-menu--inner">
-          <div class="hamburger-menu--links">
-            <div class="hamburger-menu--group">
-              <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
-                Kanaler
-              </a>
-            </div>
-            <div class="hamburger-menu--group">
-              <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
-                Slides
-              </a>
-            </div>
-            <div class="hamburger-menu--group">
-              <a href="#" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
-                Skærme
-              </a>
-            </div>
-            <div class="hamburger-menu--group">
-              <a href="/aroskanalen/style-guide" class="hamburger-menu--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
-                Style guide
-              </a>
-            </div>
-          </div>
-        </div>
       </nav>
     </div>
   </header>
