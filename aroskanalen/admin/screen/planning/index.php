8e40dfba1050e2c97fdc0478036b8ed396cd535f
Brian Jensen <hallo@magnify.dk>
2016-04-07 17:01:09 +0200

    #ARU-18 Design

--- aroskanalen/admin/screen/planning/index.php
+++ aroskanalen/admin/screen/planning/index.php
@@ -5,8 +5,6 @@
   $class = 'is-page';
 
   $submenu[0] = '<a href="#" class="submenu--link">Opret skærm</a>';
-  $submenu[1] = '<a href="' . $path_to_site . '/screen/" class="is-aligned-right">Oversigt</a>';
-  $submenu[2] = '<a href="' . $path_to_site . '/screen/planning/">Planlægning</a>';
 
   include_once $path . '/_header.php';
   include_once $path . '/_menus.php';
@@ -14,12 +12,6 @@
 
 <main role="main" class="content">
   <div class="content--inner">
-    <h1><a href="<?php echo $path_to_site; ?>/screen/">Skærme</a> / Planlægning</h1>
-    <div class="tabs is-right">
-      <a href="<?php echo $path_to_site; ?>/screen/" class="tabs--item">Oversigt</a>
-      <a href="<?php echo $path_to_site; ?>/screen/planning/" class="tabs--item is-active">Planlægning</a>
-    </div>
-    <input type="search" placeholder="Søg...">
 
   </div>
 </main>
