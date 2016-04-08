9f11ab89fa6219f814fecb3933bb1c2d24ce3ff6
Brian Jensen <hallo@magnify.dk>
2016-04-08 15:49:08 +0200

    #ARU-18 Design

--- aroskanalen/admin/screen/index.php
+++ aroskanalen/admin/screen/index.php
@@ -4,11 +4,24 @@
   $is_active = 3;
   $class = 'is-page';
 
-  $submenu[1] = '<a href="#" class="submenu--link">Opret skærm</a>';
-
   include_once $path . '/_header.php';
   include_once $path . '/_menus.php';
 ?>
+
+<nav class="submenu">
+  <div class="submenu--inner">
+    <div class="submenu--links is-left">
+      <a href="#" class="submenu--link">
+        <i class="material-icons md-36">add</i>
+        <span>Opret skærm</span>
+      </a>
+    </div>
+    <div class="submenu--links is-right">
+      <a href="<?php echo $path_to_site; ?>/screen/" class="submenu--icon-link is-active"><i class="material-icons">apps</i></a>
+      <a href="<?php echo $path_to_site; ?>/screen/planning/" class="submenu--icon-link"><i class="material-icons">event_note</i></a>
+    </div>
+  </div>
+</nav>
 
 <main role="main" class="content">
   <div class="content--inner">
--- aroskanalen/admin/screen/planning/index.php
+++ aroskanalen/admin/screen/planning/index.php
@@ -10,7 +10,16 @@
 
 <nav class="submenu">
   <div class="submenu--inner">
-    <a href="#" class="submenu--link">Opret skærm</a>
+    <div class="submenu--links is-left">
+      <a href="#" class="submenu--link">
+        <i class="material-icons md-36">add</i>
+        <span>Opret skærm</span>
+      </a>
+    </div>
+    <div class="submenu--links is-right">
+      <a href="<?php echo $path_to_site; ?>/screen/" class="submenu--icon-link"><i class="material-icons">apps</i></a>
+      <a href="<?php echo $path_to_site; ?>/screen/planning/" class="submenu--icon-link is-active"><i class="material-icons">event_note</i></a>
+    </div>
   </div>
 </nav>
 <main role="main" class="content">
