8e05fd4e8e2500f50f0d1330466cf1e4ec49add5
martinyde <yde001@gmail.com>
2016-05-03 08:51:19 +0200

    Added paragraph html

--- cfiaarhus/_header.php
+++ cfiaarhus/_header.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <link href="css/youtube.responsive.css" rel="stylesheet" type="text/css">
   <!--[if IE 9]>
   <link href="css/ie9.css" rel="stylesheet" type="text/css">
   <![endif]-->
--- cfiaarhus/om-os.php
+++ cfiaarhus/om-os.php
@@ -15,6 +15,190 @@
     <p>Hi omnes lingua, institutis, legibus inter se differunt. Quid securi etiam tamquam eu fugiat nulla pariatur. A communi observantia non est recedendum.</p>
     <p>Quam temere in vitiis, legem sancimus haerentia. Integer legentibus erat a ante historiarum dapibus. Ab illo tempore, ab est sed immemorabili.</p>
   </div>
+  <div class="paragraphs--text-left">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Ttest ttilte
+      </div>
+    </div>
+    <div class="paragraphs--text-inner">
+      <figure>  <img src="/sites/default/files/styles/paragraphs_content_image/public/content/Anne%20G.JPG?itok=xAe8wpE2" width="500" height="335" alt="test">
+      </figure>
+      <div class="paragraphs--text-left-wrapper">
+        <div class="paragraphs--text">
+          <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="paragraphs--text-left">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        ddasda
+      </div>
+    </div>
+    <div class="paragraphs--text-inner">
+      <figure>  <img src="/sites/default/files/styles/paragraphs_content_image/public/content/Anne%20G.JPG?itok=xAe8wpE2" width="500" height="335" alt="dsad">
+      </figure>
+      <div class="paragraphs--text-left-wrapper">
+        <div class="paragraphs--text">
+          <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit.Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="paragraphs--iframe">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Test iframe
+      </div>
+    </div>
+    <div class="fields--iframe">
+      <iframe width="360" height="300" src="https://www.youtube.com/embed/dIPjaniNNfo" frameborder="0" allowfullscreen=""></iframe>
+    </div>
+  </div>
+  <div class="paragraphs--iframe">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Iframe 2
+      </div>
+    </div>
+    <div class="fields--iframe">
+      <iframe width="100%" height="400" src="https://www.youtube.com/embed/mOlCgHlkIgM" frameborder="0" allowfullscreen=""></iframe>
+    </div>
+  </div>
+  <div class="paragraphs--iframe">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Yet ifram
+      </div>
+    </div>
+    <div class="fields--iframe">
+      <iframe width="100%" height="600" src="https://www.youtube.com/embed/F-PwYeYIyYI" frameborder="0" allowfullscreen=""></iframe>
+    </div>
+  </div>
+  <div class="paragraphs--internal-link">
+    <a class="figure" href="/sites/default/files/content/Anne%20K%20%282%29.JPG">
+      <figure>  <img src="/sites/default/files/content/2015-08-18%2011.07.41_0.jpg" width="3264" height="2448" alt="tets">
+      </figure>
+    </a>
+    <div class="paragraphs--internal-link-wrapper">
+      <a class="paragraphs--title" href="/sites/default/files/content/Anne%20K%20%282%29.JPG">  test
+      </a>
+      <div class="paragraphs--text">
+        <p>Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.</p>
+      </div>
+      <a href="/sites/default/files/content/Anne%20K%20%282%29.JPG">
+        Lorem
+      </a>
+    </div>
+  </div>
+  <div class="paragraphs--internal-link">
+    <a class="figure" href="/sites/default/files/content/jan-14-cool-winter-nocal-2560x1440.jpg">
+      <figure>  <img src="/sites/default/files/content/12970196_10154015752984303_669934455_o.jpeg" width="320" height="215" alt="asdf">
+      </figure>
+    </a>
+    <div class="paragraphs--internal-link-wrapper">
+      <a class="paragraphs--title" href="/sites/default/files/content/jan-14-cool-winter-nocal-2560x1440.jpg">  test
+      </a>
+      <div class="paragraphs--text">
+        <p>Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue.</p>
+        <p>Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.</p>
+      </div>
+      <a href="/sites/default/files/content/jan-14-cool-winter-nocal-2560x1440.jpg">
+        Lorem
+      </a>
+    </div>
+  </div>
+  <div class="paragraphs--internal-link">
+    <a class="figure" href="/sites/default/files/content/wicked%20problems.jpg">
+      <figure>  <img src="/sites/default/files/content/iben_sidse_nyhed.png" width="1216" height="400" alt="testsef">
+      </figure>
+    </a>
+    <div class="paragraphs--internal-link-wrapper">
+      <a class="paragraphs--title" href="/sites/default/files/content/wicked%20problems.jpg">  Lorem
+      </a>
+      <div class="paragraphs--text">
+        <p>Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.Sed posuere consectetur est at lobortis. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper.</p>
+      </div>
+      <a href="/sites/default/files/content/wicked%20problems.jpg">
+        tads
+      </a>
+    </div>
+  </div>
+  <div class="paragraphs--title-only">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Just a title
+      </div>
+    </div>
+  </div>
+  <div class="paragraphs--title-only">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Just a title
+      </div>
+    </div>
+  </div>
+  <div class="paragraphs--title-only">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Just a title
+      </div>
+    </div>
+  </div>
+  <div class="paragraphs--youtube">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Lorem
+      </div>
+    </div>
+    <figure class="youtube-container youtube-container--responsive">
+      <iframe src="https://www.youtube.com/embed/u6mwlHnb39Y?wmode=opaque" width="100%" height="100%" id="youtube-field-player" class="youtube-field-player" title="Embedded video" frameborder="0" allowfullscreen="">
+        &lt;a href="https://www.youtube.com/embed/u6mwlHnb39Y?wmode=opaque"&gt;Embedded video&lt;/a&gt;
+      </iframe>
+    </figure>
+  </div>
+  <div class="paragraphs--youtube">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        Another
+      </div>
+    </div>
+    <figure class="youtube-container youtube-container--responsive">
+      <iframe src="https://www.youtube.com/embed/u6mwlHnb39Y?wmode=opaque" width="100%" height="100%" id="youtube-field-player--2" class="youtube-field-player" title="Embedded video" frameborder="0" allowfullscreen="">
+        &lt;a href="https://www.youtube.com/embed/u6mwlHnb39Y?wmode=opaque"&gt;Embedded video&lt;/a&gt;
+      </iframe>
+    </figure>
+  </div>
+  <div class="paragraphs--youtube">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        A last
+      </div>
+    </div>
+    <figure class="youtube-container youtube-container--responsive">
+      <iframe src="https://www.youtube.com/embed/u6mwlHnb39Y?wmode=opaque" width="100%" height="100%" id="youtube-field-player--3" class="youtube-field-player" title="Embedded video" frameborder="0" allowfullscreen="">
+        &lt;a href="https://www.youtube.com/embed/u6mwlHnb39Y?wmode=opaque"&gt;Embedded video&lt;/a&gt;
+      </iframe>
+    </figure>
+  </div>
+  <div class="paragraphs--text-left">
+    <div class="paragraphs--title">
+      <div class="paragraphs--title-item">
+        testsets
+      </div>
+    </div>
+    <div class="paragraphs--text-inner">
+      <figure>  <img src="/sites/default/files/styles/paragraphs_content_image/public/content/151217_DesignCamp_panel_RED%5B1%5D.jpg?itok=Ypc4ETGM" width="500" height="164" alt="test">
+      </figure>
+      <div class="paragraphs--text-left-wrapper">
+        <div class="paragraphs--text">
+          <p>Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit. Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Nullam quis risus eget urna mollis ornare vel eu leo. Nulla vitae elit libero, a pharetra augue. Donec ullamcorper nulla non metus auctor fringilla. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="itk-boxlist">
     <article class="itk-boxlist--item">
       <header>
