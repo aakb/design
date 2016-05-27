a5cd99b002d478093e01447cb7a4ef0d570d1853
martinyde <yde001@gmail.com>
2016-05-27 13:58:09 +0200

    GS-12 by martinyde: Added styling

---
+++ grafisk-service/thank-you/_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Grafisk Service</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../css/styles.css" rel="stylesheet" type="text/css">
+  <!--[if IE 9]>
+  <link href="../css/ie9.css" rel="stylesheet" type="text/css">
+  <![endif]-->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="../js/progress.js"></script>
+  <script src="../js/filefield.js"></script>
+  <script src="../js/modal.js"></script>
+</head>
