e5f7264334a2f60da18ba46dae9896b841b472dd
martinyde <yde001@gmail.com>
2016-05-30 10:33:51 +0200

    Added gallery styling

--- dokkx/_footer.php
+++ dokkx/_footer.php
@@ -3,12 +3,20 @@
   <div class="footer--content">
     <!--block.html.twig START-->
     <div class="footer--aarhus">
-      <a href="http://aarhus.dk"><figure class="footer--logo"><img src="/dokkx/images/aak-logo.png"></figure></a>
+      <a href="http://aarhus.dk"><figure class="footer--logo"><img src="/cfiaarhus/images/AAK-02-venstre-neg.png"></figure></a>
     </div>
     <div class="footer--mail">
-      <p>Dokk1 niveau 2, Hack Kampmanns Plads 2 8000 Aarhus C - <a href="mailto:#">[Mangler e-mail]</a></p>
+      <p>Dokk1, Hack Kampmanns Plads 2 8000 Aarhus C - <a href="mailto:cfi@aarhus.dk">cfi@aarhus.dk</a></p>
     </div>
     <!--block.html.twig END-->
+    <!--block.html.twig START-->
+    <div class="footer--social">
+      <a href="#"><figure class="footer--icons"><img src="images/assets/twitter.png"></figure></a>
+      <a href="#"><figure class="footer--icons"><img src="images/assets/facebook.png"></figure></a>
+      <a href="#"><figure class="footer--icons"><img src="images/assets/linkedin.png"></figure></a>
+    </div>
+    <!--block.html.twig END-->
+
   </div>
 </footer>
 <!--region.html.twig END-->
