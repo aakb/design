90be28b493e5ec30fbba65f3de544a858a9f7c47
Brian Jensen <hallo@magnify.dk>
2016-06-07 14:38:29 +0200

    #ITKORE-42 Replaced itkore css and html from 5pack

--- itkore/_menus.php
+++ itkore/_menus.php
@@ -1,16 +1,17 @@
 <!--html.html.twig START-->
   <!--page.html.twig START-->
 <body class="<?php echo $class; ?>">
-  <a href="/itkore" class="logo js-logo" title="Iore">
-    <?php include $path . '/images/assets/logo.svg'; ?>
-  </a>
+  <div class="overlay js-menu-toggle"></div>
+  <a href="<?php echo $path_to_site; ?>/" class="logo js-logo" title=""><span class="logo--text">ITKore</span></a>
   <!--menu.html.twig START-->
   <nav class="nav js-nav">
     <div class="nav--links">
-      <a class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>" href="/itkore/news/">News</a>
-      <a class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>" href="/itkore/events/">Events</a>
-      <a class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>" href="/itkore/om-dokkx/">Om DokkX</a>
-      <a class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>" href="/itkore/kontakt/">Kontakt</a>
+      <a class="nav--link<?php echo $is_active === 0 || $is_sub_active[0] ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/om-5-pack/">Om 5 pack</a>
+      <a class="nav--link<?php echo $is_active === 1 || $is_sub_active[1] ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/nyheder/">Nyheder</a>
+      <a class="nav--link<?php echo $is_active === 2 || $is_sub_active[2] ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/temaer/">Temaer</a>
+      <a class="nav--link<?php echo $is_active === 3 || $is_sub_active[3] ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/netvaerk/">Netværk</a>
+      <a class="nav--link<?php echo $is_active === 4 || $is_sub_active[4] ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/kontakt/">Kontakt</a>
+      <a class="nav--link<?php echo $is_active === 5 || $is_sub_active[5] ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/english/">English</a>
     </div>
   </nav>
   <!--menu.html.twig END-->
@@ -21,47 +22,58 @@
     <span></span>
   </div>
   <!--menu.html.twig START-->
-  <div class="hamburger-menu--overlay js-menu-toggle">
-    <nav class="hamburger-menu">
-      <div class="hamburger-menu--inner">
-        <div class="hamburger-menu--links">
-          <div class="hamburger-menu--group<?php echo $is_group_active === 0 || isset($is_sub_active[0]) ? ' is-active' : '' ?>">
-            <a href="/itkore/aktiviteter/" class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
-              Aktiviteter
-            </a>
-            <a class="nav--link is-sub <?php echo $is_sub_active[0] === 0 ? ' is-active' : '' ?>" href="/itkore/aktiviteter/sundhed-traening.php">
-              Sundhed og træning
-            </a>
-            <a class="nav--link is-sub <?php echo $is_sub_active[0] === 1 ? ' is-active' : '' ?>" href="/itkore/aktiviteter/book-rundvisning.php">
-              Book en rundvisning
-            </a>
-            <a class="nav--link is-sub <?php echo $is_sub_active[0] === 2 ? ' is-active' : '' ?>" href="/itkore/aktiviteter/teknologier.php">
-              Teknologier
-            </a>
-            <a class="nav--link is-sub <?php echo $is_sub_active[0] === 3 ? ' is-active' : '' ?>" href="/itkore/aktiviteter/arkiv.php">
-              Arkiv
-            </a>
-          </div>
-          <div class="hamburger-menu--group<?php echo $is_group_active === 1 || isset($is_sub_active[1]) ? ' is-active' : '' ?>">
-            <a href="/itkore/besoeg-dokkx/" class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">
-              Besøg DokkX
-            </a>
-            <a class="nav--link is-sub <?php echo $is_sub_active[1] === 0 ? ' is-active' : '' ?>" href="/itkore/besoeg-dokkx/aabningstider.php">
-              Åbningstider</a>
-          </div>
-          <div class="hamburger-menu--group<?php echo $is_group_active === 2 || isset($is_sub_active[1]) ? ' is-active' : '' ?>">
-            <a href="/itkore/om-dokkx/" class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">
-              Om DokkX
-            </a>
-          </div>
-          <div class="hamburger-menu--group<?php echo $is_group_active === 3 || isset($is_sub_active[1]) ? ' is-active' : '' ?>">
-            <a href="/itkore/designguide.php" class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">
-              Designguide
-            </a>
-          </div>
+  <nav class="hamburger-menu">
+    <div class="hamburger-menu--inner">
+      <div class="hamburger-menu--links">
+        <div class="hamburger-menu--group">
+          <a href="<?php echo $path_to_site; ?>/om-5-pack/" class="nav--link<?php echo $is_active === 0 ? ' is-active' : '' ?>">
+            Om 5 pack
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="<?php echo $path_to_site; ?>/nyheder/" class="nav--link<?php echo $is_active === 1 ? ' is-active' : '' ?>">
+            Nyheder
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="<?php echo $path_to_site; ?>/temaer/" class="nav--link<?php echo $is_active === 2 ? ' is-active' : '' ?>">
+            Temaer
+          </a>
+          <a class="nav--link is-sub <?php echo $is_sub_active[2] === 1 ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/temaer/den-digitale-infrastruktur/">
+            Den digtiale infrastruktur
+          </a>
+          <a class="nav--link is-sub <?php echo $is_sub_active[2] === 2 ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/temaer/open-data/">
+            Open Data
+          </a>
+          <a class="nav--link is-sub <?php echo $is_sub_active[2] === 3 ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/temaer/mobilitet/">
+            Mobilitet
+          </a>
+          <a class="nav--link is-sub <?php echo $is_sub_active[2] === 4 ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/temaer/forsyning/">
+            Forsyning
+          </a>
+          <a class="nav--link is-sub <?php echo $is_sub_active[2] === 5 ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/temaer/mennesker/">
+            Mennesker
+           </a>
+          <a class="nav--link is-sub <?php echo $is_sub_active[2] === 6 ? ' is-active' : '' ?>" href="<?php echo $path_to_site; ?>/temaer/living-labs/">
+            Living Labs
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="<?php echo $path_to_site; ?>/netvaerk/" class="nav--link<?php echo $is_active === 3 ? ' is-active' : '' ?>">
+            Netværk
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="<?php echo $path_to_site; ?>/kontakt/" class="nav--link<?php echo $is_active === 4 ? ' is-active' : '' ?>">
+            Kontakt
+          </a>
+        </div>
+        <div class="hamburger-menu--group">
+          <a href="<?php echo $path_to_site; ?>/english/" class="nav--link<?php echo $is_active === 5 ? ' is-active' : '' ?>">
+            English
+          </a>
         </div>
       </div>
-    </nav>
-  </div>
-
+    </div>
+  </nav>
   <!--menu.html.twig END-->
