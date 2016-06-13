e5d57224a2bbb51a3cd0c1d698691a05081ec68f
Jesper Kristensen <cableman@linuxdev.dk>
2016-06-13 16:56:45 +0200

    Issue #MKBDIGI-64: Tried to download images

--- itkscreen/images/download.js
+++ itkscreen/images/download.js
@@ -1,43 +1,40 @@
-var fs = require('fs'),
-  request = require('request');
+var fs = require('fs');
+var ntlm = require('request-ntlm');
 
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
 
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+
+var download = function(az){
+  var httpntlm = require('httpntlm');
+
+  var f=fs.createWriteStream(az + '.jpg');
+
+  httpntlm.get({
+    url: 'http://srvwebaak01/TLFimageupload/imageHandler.ashx?ident=' + az,
+    encoding: null,
+    username: 'azkbd85',
+    password: 'Fn)33>mp=d',
+    workstation: '',
+    domain: ''
+  }, function (err, res){
+    if(err) return err;
+    res.setEncoding('binary');
+    
+    console.log(res);
+    fs.writeFile(az + '.jpg', res.body, 'binary', function(err){
+      if (err) throw err;
+      console.log('File saved.')
+    })
   });
 };
 
-download('https://www.google.com/images/srpr/logo3w.png', 'google.png', function(){
-  console.log('done');
-});var fs = require('fs'),
-  request = require('request');
 
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
 
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
+var json = require('../data.json');
 
-download('https://www.google.com/images/srpr/logo3w.png', 'google.png', function(){
-  console.log('done');
-});var fs = require('fs'),
-  request = require('request');
+for (var i in json) {
+  for (var j in json[i].members.data) {
+    var az = json[i].members.data[j].azident.trim();
+    download(az);
+  }
+}
 
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    console.log('content-type:', res.headers['content-type']);
-    console.log('content-length:', res.headers['content-length']);
-
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
-
-download('https://www.google.com/images/srpr/logo3w.png', 'google.png', function(){
-  console.log('done');
-});
--- itkscreen/js/dataService.js
+++ itkscreen/js/dataService.js
@@ -2,5 +2,13 @@ angular.module('groupApp').service('dataService', ['$http', '$q',
   function ($http, $q) {
     "use strict";
 
+    /**
+     * Load json file.
+     *
+     * @returns {*|{}}
+     */
+    this.getData = function getData() {
+      return $http.get('data.json');
+    }
   }
 ]);
